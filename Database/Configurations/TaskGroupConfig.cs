5e68bb6a5bc016b1009b9cb78534617db7fbf9fe
Ivan <ivan.andrau@gmail.com>
2026-03-12 23:52:12 +0100

    feat: implement config files for TaskGroup and TaskGroupMember

---
+++ Database/Configurations/TaskGroupConfig.cs
@@ -0,0 +1,35 @@
+﻿using HabitTracker.DataModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HabitTracker.Database.Configurations
+{
+    public class TaskGroupConfig : IEntityTypeConfiguration<TaskGroup>
+    {
+        public void Configure(EntityTypeBuilder<TaskGroup> builder)
+        {
+
+            builder.HasKey(tg => tg.Id);
+
+            builder.Property(tg => tg.Title)
+                .HasMaxLength(64)
+                .IsRequired();
+
+            builder.Property(tg => tg.Color)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(tg => tg.Icon)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(tg => tg.CreatedById)
+                .IsRequired();
+
+            builder.HasOne(tg => tg.CreatedBy)
+                .WithMany(u => u.CreatedTaskGroups)
+                .HasForeignKey(tg => tg.CreatedById)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Database/Configurations/TaskGroupMemberConfig.cs
@@ -0,0 +1,34 @@
+﻿using HabitTracker.DataModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HabitTracker.Database.Configurations
+{
+    public class TaskGroupMemberConfig : IEntityTypeConfiguration<TaskGroupMember>
+    {
+        public void Configure(EntityTypeBuilder<TaskGroupMember> builder)
+        {
+            builder.HasKey(tgm => new { tgm.TaskGroupId, tgm.UserId }); // Fixed composite PK
+
+            builder.Property(tgm => tgm.TaskGroupId)
+                .IsRequired();
+
+            builder.Property(tgm => tgm.UserId)
+                .IsRequired();
+
+            builder.Property(tgm => tgm.JoinedAt)
+                .HasDefaultValueSql("CURRENT_DATE")
+                .IsRequired();
+
+            builder.HasOne(tgm => tgm.TaskGroup)
+                .WithMany(tg => tg.Members)
+                .HasForeignKey(tgm => tgm.TaskGroupId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(tgm => tgm.User)
+                .WithMany(u => u.TaskGroupMemberships)
+                .HasForeignKey(tgm => tgm.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
